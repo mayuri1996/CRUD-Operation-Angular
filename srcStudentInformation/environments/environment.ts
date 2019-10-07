a62f8d6562ee7bbb314b606d241292e5d1033f98
mayuri1996 <45028313+mayuri1996@users.noreply.github.com>
2019-10-07 12:11:42 +0530

    hide details

--- srcStudentInformation/environments/environment.ts
+++ srcStudentInformation/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "student",
-    authDomain: "studentdetails-e23d7.firebaseapp.com",
-    databaseURL: "https://studentdetails-e23d7.firebaseio.com/",
-    projectId: "studentdetails-e23d7",
-    storageBucket: "studentdetails-e23d7.appspot.com",
-    messagingSenderId: "9579842845"
+    apiKey: "-----",
+    authDomain: "----",
+    databaseURL: "-----",
+    projectId: "----",
+    storageBucket: "----",
+    messagingSenderId: "----"
   }
 };
 
